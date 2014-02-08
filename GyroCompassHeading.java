fe3e82be9fd9df38faef38565e026b1b31791a0d
Jamari <jamariducre@gmail.com>
2014-02-08 09:49:54 -0800

    Create GyroCompassHeading.java

---
+++ GyroCompassHeading.java
@@ -0,0 +1,27 @@
+   public double getGyroCompassHeading(){
+    
+    /*
+    Updates and returns the heading.
+    Heading is not tilt compensated.
+    Dependent on a working gyro. 
+    */
+    
+        Gyro.read();
+                
+        if(i < 20){
+        gyroArray[i] = Gyro.getAxisZ();
+        i++;
+        }else {
+        i = 0;
+        }
+        double average = 0;
+        for(int j = 0; j< gyroArray.length; j++){
+        
+        average += gyroArray[j];
+        }
+        average /= 20;
+        
+        heading = ((heading + (average - headingOffset)*2.25*.06)) %360;
+        System.out.println(average);
+        return heading;
+    }
